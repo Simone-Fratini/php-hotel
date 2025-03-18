0844bd6294583b4b71de396ccc2e5f9cd38aa3e0
Simone-Fratini <simone@fratini.com>
2025-03-18 18:47:27 +0100

    Refactor index.php to include a form for user input and wrap content in a container for better layout.

--- index.php
+++ index.php
@@ -53,48 +53,57 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <h1 class="text-success text-center pt-2">PHP-HOTEL</h1>
+    <div class="container">
+        <h1 class="text-success text-center pt-2">PHP-HOTEL</h1>
 
-    <table class="table table-striped">
+        <form action="">
+            <input type="checkbox" name="" id="">
+            <label for=""></label>
 
-      
-        <thead>
-            <tr>
-            <th scope="col">N.</th>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Parking</th>
-            <th scope="col">Vote</th>
-            <th scope="col">Distance to center</th>
-            </tr>
-        </thead>
-        <tbody>
+            <input type="number" name="vote" id="vote" min="0" max="10">
+            <label for="vote"></label>
+        </form>
 
-        <?php
-            foreach($hotels as $hotel){
-        ?>
-            <tr>
-            <th scope="row"><?php echo $counter ?></th>
-            <td><?php echo $hotel["name"] ?></td>
-            <td><?php echo $hotel["description"] ?></td>
-            <td><?php echo $hotel["parking"] == true ? "yes" : "no" ?></td>
-            <td><?php echo $hotel["vote"] ?></td>
-            <td><?php echo $hotel["distance_to_center"]."km" ?></td>
-            
-            </tr>
-            
+        <table class="table table-striped">
 
-        <?php
-            $counter++;
-            }
-        ?>
-        </tbody>
-    
+        
+            <thead>
+                <tr>
+                <th scope="col">N.</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <?php
+                foreach($hotels as $hotel){
+            ?>
+                <tr>
+                <th scope="row"><?php echo $counter ?></th>
+                <td><?php echo $hotel["name"] ?></td>
+                <td><?php echo $hotel["description"] ?></td>
+                <td><?php echo $hotel["parking"] == true ? "yes" : "no" ?></td>
+                <td><?php echo $hotel["vote"] ?></td>
+                <td><?php echo $hotel["distance_to_center"]."km" ?></td>
+                
+                </tr>
+                
+
+            <?php
+                $counter++;
+                }
+            ?>
+            </tbody>
+        
 
 
 
-    </table>
-
+        </table>
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
